a2d06ae5f86339ed20ae4044f88711f59f667bf4
Tomas <tomibudeguer20@gmail.com>
2022-10-25 20:23:34 -0300

    hice la funcionalidad para listar todas las tareas

--- src/controllers/tarea.controllers.js
+++ src/controllers/tarea.controllers.js
@@ -1,7 +1,15 @@
 import Tarea from "../models/tarea";
 
-export const listarTareas = (req, res) => {
-  res.send("Esto es una prueba de la peticion GET");
+export const listarTareas = async(req, res) => {
+    try {
+        const listaTareas = await Tarea.find();
+        res.status(200).json(listaTareas);
+      } catch (error) {
+        console.log(error);
+        res.status(404).json({
+          mensaje: "Error al intentar agregar tarea",
+        });
+      }
 };
 
 export const crearTarea = async (req, res) => {
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,9 +1,9 @@
-import {Router} from "express"
-import { crearTarea, listarTareas } from "../controllers/tarea.controllers"
+import { Router } from "express";
+import { crearTarea, listarTareas } from "../controllers/tarea.controllers";
 
-const router = Router()
+const router = Router();
 
-router.route('/tareas').get(listarTareas).post(crearTarea)
+router.route("/tareas").get(listarTareas).post(crearTarea);
+router.route("/tareas/:id").get().put().delete();
 
-export default router
-
+export default router;
