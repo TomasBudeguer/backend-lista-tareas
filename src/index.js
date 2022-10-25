41ba062c7b14e01343ccb36f8d864370b6fb1d72
Tomas <tomibudeguer20@gmail.com>
2022-10-25 19:54:05 -0300

    conecte el proyecto a la base de datos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import path from 'path'
+import './database'
 const app = express()
 
 app.set('port', process.env.PORT || 4000)
@@ -14,7 +15,7 @@ app.listen(app.get("port"), ()=>{
 
 app.use(morgan('dev'))
 app.use(cors())
-app.use(express.json)
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, '../public')))
 
